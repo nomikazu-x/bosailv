d559406f1632a54786d3b99b1459f6da35a0c40c
kazuki-ayimon <nomikazyu@gmail.com>
2022-03-27 11:22:05 +0900

    feat: create nuxt

---
+++ front/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  overrides: [
+    {
+      files: ["*.vue"],
+      processor: "vue/.vue"
+    }
+  ],
+  // add your custom rules here
+  rules: {
+  }
+}
