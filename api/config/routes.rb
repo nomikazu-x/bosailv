bc0025b72588d067da403ebe29a38ff9d5d7980c
kazuki-ayimon <nomikazyu@gmail.com>
2022-03-29 21:01:41 +0900

    feat: ユーザー認証に必要なapiの設定をした

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api do
-    namespace :v1 do
-      # api test action
-      resources :hello, only: [:index]
-    end
+    mount_devise_token_auth_for 'User', at: 'auth'
   end
 end
