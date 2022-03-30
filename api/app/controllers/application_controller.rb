3488fe031a4362d38f69145667e4ec21aa2de702
kazuki-ayimon <nomikazyu@gmail.com>
2022-03-30 19:24:02 +0900

    fix: Application_controllerを修正

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, if: :devise_controller?, raise: false 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
