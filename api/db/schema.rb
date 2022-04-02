99fb130350de03baca47a0d17f649d2c82d3e700
kazuki-ayimon <nomikazyu@gmail.com>
2022-04-02 15:53:40 +0900

    feat: Userモデルにadminカラムを追加した #9

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_29_115307) do
+ActiveRecord::Schema.define(version: 2022_04_02_064250) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2022_03_29_115307) do
     t.text "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
