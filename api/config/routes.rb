c46d665845818fb609185fbcf42d7f27fe2ac345
kazuki-ayimon <nomikazyu@gmail.com>
2022-04-02 18:03:16 +0900

    feat: rails db:migrate #9

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admin_users
+  devise_for :admin_users, controllers: {
+    sessions: 'admin_users/sessions'
+  }
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   namespace :api do
