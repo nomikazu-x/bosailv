bb2c982c58860257d63d0cea75bfc05bd3acce77
kazuki-ayimon <nomikazyu@gmail.com>
2022-04-03 00:19:55 +0900

    feat: rspecの設定を追記 #13

--- api/spec/rails_helper.rb
+++ api/spec/rails_helper.rb
@@ -7,6 +7,9 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+# Use devise
+require 'devise'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -61,4 +64,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Use devise
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include FactoryBot::Syntax::Methods
 end
