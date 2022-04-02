cbc8b0c901041467b74f49e2534c3b8dd0c359b3
kazuki-ayimon <nomikazyu@gmail.com>
2022-04-02 17:12:42 +0900

    feat: rails-adminで管理画面を作成した #9

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   skip_before_action :verify_authenticity_token, if: :devise_controller?, raise: false 
   before_action :configure_permitted_parameters, if: :devise_controller?
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   namespace :api do
     mount_devise_token_auth_for 'User', at: 'auth'
