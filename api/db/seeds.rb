6d6145326bc2ef7a662b81bbbb46cdb5a47dc346
kazuki-ayimon <nomikazyu@gmail.com>
2022-04-02 19:55:45 +0900

    feat: 初期設定をseedで投入 #11

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Dir.glob("#{Rails.root}/db/seed/*.yml").each do |filename|
+  puts filename
+
+  target_model = File.basename(filename, '.yml').classify.constantize
+  puts target_model.to_s
+
+  File.open(filename) do |file_contents|
+    yaml_contents = YAML.safe_load(file_contents)
+    yaml_contents.each do |yaml_record|
+      id = yaml_record['id']
+
+      if target_model.find_by(id: id)
+        puts id.to_s
+        next
+      end
+
+      puts id.to_s
+      target_model.create(yaml_record)
+    end
+  end
+end
