19bce2a4e400564defd5659b7a3ca3f46f4cbbc4
kazuki-ayimon <nomikazyu@gmail.com>
2022-04-02 20:15:13 +0900

    feat: 開発環境のみユーザーの初期アカウントを作成できるようにした #11

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,21 +1,24 @@
-Dir.glob("#{Rails.root}/db/seed/*.yml").each do |filename|
-  puts "filename: #{filename}"
+paths = ['', "#{Rails.env}/"]
+paths.each do |path|
+  Dir.glob("#{Rails.root}/db/seed/#{path}*.yml").each do |filename|
+    puts "filename: #{filename}"
 
-  target_model = File.basename(filename, '.yml').classify.constantize
-  puts "model: #{target_model}"
+    target_model = File.basename(filename, '.yml').classify.constantize
+    puts "model: #{target_model}"
 
-  File.open(filename) do |file_contents|
-    yaml_contents = YAML.safe_load(file_contents)
-    yaml_contents.each do |yaml_record|
-      id = yaml_record['id']
+    File.open(filename) do |file_contents|
+      yaml_contents = YAML.safe_load(file_contents)
+      yaml_contents.each do |yaml_record|
+        id = yaml_record['id']
 
-      if target_model.find_by(id: id)
-        puts "id: #{id} Skip create"
-        next
+        if target_model.find_by(id: id)
+          puts "id: #{id} Skip create"
+          next
+        end
+
+        puts "id: #{id} Create"
+        target_model.create(yaml_record)
       end
-
-      puts "id: #{id} Create"
-      target_model.create(yaml_record)
     end
   end
 end
