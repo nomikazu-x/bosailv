82bf36f8bebb37eca25f2cdd6c574fe11413e07c
kazuki-ayimon <nomikazyu@gmail.com>
2022-04-03 20:02:10 +0900

    feat: letter_opener_webの追加、メール確認できるようにした #16

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   namespace :api do
     mount_devise_token_auth_for 'User', at: 'auth'
   end
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
