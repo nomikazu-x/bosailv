290b4d1483b3c1d56000a04af2f8056ceb46012b
kazuki-ayimon <nomikazyu@gmail.com>
2022-04-04 21:16:43 +0900

    feat: FactoryBotの設定 #16

--- api/spec/rails_helper.rb
+++ api/spec/rails_helper.rb
@@ -1,5 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'faker'
+Faker::Config.locale = :ja
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
