74f2ef7dcf4541f7240b292ab7d4be0ae048cdfa
kazuki-ayimon <nomikazyu@gmail.com>
2022-04-05 21:40:18 +0900

    feat: ユーザーテーブルにusername,profile,addressカラムを追加 #16

---
+++ api/db/migrate/20220405123428_add_profile_and_address_and_username_to_users.rb
@@ -0,0 +1,17 @@
+class AddProfileAndAddressAndUsernameToUsers < ActiveRecord::Migration[6.1]
+  def up
+    remove_column :users, :nickname
+
+    add_column :users, :address, :string, limit: 30
+    add_column :users, :profile, :string
+    add_column :users, :username, :string, limit: 30
+  end
+
+  def down
+    add_column :users, :nickname, :string
+
+    remove_column :users, :address, :string
+    remove_column :users, :profile, :string
+    remove_column :users, :username
+  end
+end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_02_064250) do
+ActiveRecord::Schema.define(version: 2022_04_05_123428) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -30,13 +30,15 @@ ActiveRecord::Schema.define(version: 2022_04_02_064250) do
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.string "name"
-    t.string "nickname"
     t.string "image"
     t.string "email"
     t.text "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false, null: false
+    t.string "address", limit: 30
+    t.string "profile"
+    t.string "username", limit: 30
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
