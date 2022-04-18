10cec1c9b1019b9a817ee2c9b7a85a0d693289a5
kazuki-ayimon <nomikazyu@gmail.com>
2022-04-19 00:09:19 +0900

    style: header,sidebarのデザインを修正 #28

--- front/components/TheHeader.vue
+++ front/components/TheHeader.vue
@@ -2,26 +2,62 @@
   <v-app-bar app clipped-left color="amber lighten-2" dense fixed>
     <v-app-bar-nav-icon @click="onClick()" />
 
-    <v-toolbar-title>
-      <NuxtLink to="/" class="text--secondary text-decoration-none">
-        <h2>App</h2>
-      </NuxtLink>
-    </v-toolbar-title>
+    <NuxtLink to="/" class="toolbar-title d-flex text-decoration-none text--secondary">
+      <v-img src="/v.png" max-width="40px" max-height="40px" />
+      <v-toolbar-title
+        v-if="$vuetify.breakpoint.width > 226"
+        :style="{ 'max-width': ($vuetify.breakpoint.width - 226) + 'px' }"
+        class="ml-1 align-self-center d-inline-block text-truncate"
+      >
+        <h2>app</h2>
+      </v-toolbar-title>
+    </NuxtLink>
     <v-spacer />
-
-    <keep-alive>
-      <div v-if="$auth.loggedIn">
-        <client-only>
-          <v-btn large nuxt color="primary">ログアウト</v-btn>
-        </client-only>
-      </div>
-
-      <div v-else>
-        <client-only>
-          <v-btn large nuxt to="/users/sign_in" color="primary">ログイン</v-btn>
-        </client-only>
-      </div>
-    </keep-alive>
+    <template v-if="!$auth.loggedIn">
+      <v-btn to="/users/sign_in" text rounded exact nuxt>
+        <v-icon>mdi-login</v-icon>
+        <div class="hidden-sm-and-down">ログイン</div>
+      </v-btn>
+      <v-btn to="/users/sign_up" text rounded exact nuxt>
+        <v-icon>mdi-account-plus</v-icon>
+        <div class="hidden-sm-and-down">アカウント登録</div>
+      </v-btn>
+    </template>
+    <template v-else>
+      <v-menu offset-y>
+        <template #activator="{ on, attrs }">
+          <v-btn class="d-inline-block" max-width="400px" style="text-transform: none" text v-bind="attrs" v-on="on">
+            <v-avatar size="32px">
+              <v-img id="user_image" :src="$auth.user.image_url.small" />
+            </v-avatar>
+            <div class="text-truncate hidden-sm-and-down">{{ $auth.user.name }}</div>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item to="/users/edit" exact nuxt>
+            <v-list-item-icon>
+              <v-icon>mdi-account-edit</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>登録情報変更</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/users/sign_out" exact nuxt>
+            <v-list-item-icon>
+              <v-icon>mdi-logout</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>ログアウト</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-btn to="/" text rounded exact nuxt>
+        <v-badge color="red" overlap>
+          <v-icon>mdi-bell</v-icon>
+        </v-badge>
+      </v-btn>
+    </template>
   </v-app-bar>
 </template>
 
