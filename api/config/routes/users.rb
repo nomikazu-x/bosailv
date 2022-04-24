3b17dbf2f1da2c965d432c42bb7ab9c93406d222
kazuki-ayimon <nomikazyu@gmail.com>
2022-04-24 20:46:36 +0900

    refactor: routesの記法を修正

---
+++ api/config/routes/users.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users, skip: :all
+  devise_scope :user do
+    # Devise Token Auth
+    post 'users/auth/sign_up',         to: 'users/auth/registrations#create',             as: 'create_user_auth_registration'
+    get  'users/auth/show',            to: 'users/auth/registrations#show',               as: 'show_user_auth_registration'
+    post 'users/auth/update',          to: 'users/auth/registrations#update',             as: 'update_user_auth_registration'
+    post 'users/auth/image/update',    to: 'users/auth/registrations#image_update',       as: 'update_user_auth_image_registration'
+    post 'users/auth/image/delete',    to: 'users/auth/registrations#image_destroy',      as: 'delete_user_auth_image_registration'
+    post 'users/auth/delete',          to: 'users/auth/registrations#destroy',            as: 'destroy_user_auth_registration'
+    post 'users/auth/sign_in',         to: 'users/auth/sessions#create',                  as: 'create_user_auth_session'
+    post 'users/auth/sign_out',        to: 'users/auth/sessions#destroy',                 as: 'destroy_user_auth_session'
+    get  'users/auth/validate_token',  to: 'users/auth/token_validations#validate_token', as: 'user_auth_validate_token'
+  end
+end
