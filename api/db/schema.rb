bd34095a8b7dc75d36ce97aa66ae8a6f51f44cc9
kazuki-ayimon <nomikazyu@gmail.com>
2022-05-24 21:10:59 +0900

    feat: usersテーブルにレベル関係のカラムを追加 #41

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_144153) do
+ActiveRecord::Schema.define(version: 2022_05_24_115744) do
 
   create_table "article_comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -90,6 +90,9 @@ ActiveRecord::Schema.define(version: 2022_05_23_144153) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false, null: false
     t.datetime "infomation_check_last_started_at"
+    t.integer "level", default: 1, null: false
+    t.integer "lifelong_point", default: 0, null: false
+    t.integer "point_to_next", default: 5, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
