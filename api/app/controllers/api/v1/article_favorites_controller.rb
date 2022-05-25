b9b29ca7f50d1761f1c59795df69f27f546cc284
kazuki-ayimon <nomikazyu@gmail.com>
2022-05-25 18:18:41 +0900

    fix: お気に入り時にトランザクションを追加 #41

--- api/app/controllers/api/v1/article_favorites_controller.rb
+++ api/app/controllers/api/v1/article_favorites_controller.rb
@@ -5,9 +5,12 @@ class Api::V1::ArticleFavoritesController < Api::V1::ApplicationController
     article = Article.find(params[:article_id])
 
     ActiveRecord::Base.transaction do
-      current_user.article_favorite!(article)
-      Infomation.new(started_at: Time.current, target: :User, user_id: article.user.id,
-                     action: 'ArticleFavorite', action_user_id: current_user.id, article_id: article.id).save!
+      if current_user.article_favorite!(article)
+        Infomation.new(started_at: Time.current, target: :User, user_id: article.user.id,
+                      action: 'ArticleFavorite', action_user_id: current_user.id, article_id: article.id).save!
+      else
+        render './api/v1/failure', locals: { alert: I18n.t('alert.article_favorite.create') }, status: :unprocessable_entity
+      end
     end
 
     render json: { notice: I18n.t('notice.article_favorite.create') }
@@ -15,8 +18,12 @@ class Api::V1::ArticleFavoritesController < Api::V1::ApplicationController
 
   def destroy
     article = Article.find(params[:article_id])
-    current_user.article_unfavorite!(article)
-
-    render json: { notice: I18n.t('notice.article_favorite.destroy') }
+    ActiveRecord::Base.transaction do
+      if current_user.article_unfavorite!(article)
+        render json: { notice: I18n.t('notice.article_favorite.destroy') }
+      else
+        render './api/v1/failure', locals: { alert: I18n.t('alert.article_favorite.destroy') }, status: :unprocessable_entity
+      end
+    end
   end
 end
