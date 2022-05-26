5d1319364cdd83d6c22223f7655f551f12fadb46
kazuki-ayimon <nomikazyu@gmail.com>
2022-05-26 23:53:27 +0900

    feat: ボタンを追加 #41

---
+++ front/components/atoms/btns/GreenBtn.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-btn
+    class="btn"
+    depressed
+    :disabled="disabled"
+    :large="large"
+    :to="to"
+    :color="color"
+    @click="onClick"
+  >
+    <slot />
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: 'green'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    large: {
+      type: Boolean,
+      default: false
+    },
+    to: {
+      type: [String, Object],
+      default: undefined
+    }
+  },
+  methods: {
+    onClick () {
+      return this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  color: white;
+}
+</style>
