875681379568521b2f1be67ba458377ded2b3478
kazuki-ayimon <nomikazyu@gmail.com>
2022-05-28 12:02:03 +0900

    feat: 赤ボタンを追加 #41

---
+++ front/components/atoms/btns/RedBtn.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-btn
+    color="red lighten-2"
+    class="btn"
+    :disabled="disabled"
+    :large="large"
+    :to="to"
+    depressed
+    @click="onClick"
+  >
+    <slot />
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    large: {
+      type: Boolean,
+      default: false
+    },
+    to: {
+      type: [String, Object],
+      default: undefined
+    }
+  },
+  methods: {
+    onClick () {
+      return this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  color: white;
+}
+</style>
