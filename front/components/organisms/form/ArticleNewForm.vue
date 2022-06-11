e748910636b5ba7412d81aeeea4695457013ff81
kazuki-ayimon <nomikazyu@gmail.com>
2022-06-11 21:35:44 +0900

    refactor: 記事一覧ページをリファクタリング #51

---
+++ front/components/organisms/form/ArticleNewForm.vue
@@ -0,0 +1,98 @@
+<template>
+  <ValidationObserver v-slot="{ invalid }" ref="observer">
+    <Processing v-if="processing" />
+    <v-form autocomplete="off">
+      <v-row justify="center">
+        <v-col class="py-0 mr-sm-4" cols="12" sm="8" md="8">
+          <ArticleThumbnailFileInput
+            v-model="thumbnail"
+            :url="url"
+          />
+        </v-col>
+        <v-col class="py-0 mr-sm-4" cols="12" sm="8" md="8">
+          <ArticleTitleTextField
+            v-model="title"
+          />
+        </v-col>
+        <v-col class="mb-16" cols="12" sm="8" md="8">
+          <ArticleCategoryCheckbox v-model="selectedCategories" />
+        </v-col>
+        <v-col class="py-0 mr-sm-4" cols="12" sm="8" md="8">
+          <Editor
+            v-model="content"
+          />
+        </v-col>
+        <div class="text-right">
+          <OrangeBtn
+            id="article_create_btn"
+            class="post-btn"
+            :disabled="invalid || processing"
+            @click="onArticleCreate"
+          >
+            作成
+          </OrangeBtn>
+        </div>
+      </v-row>
+    </v-form>
+  </ValidationObserver>
+</template>
+
+<script>
+export default {
+  props: {
+    errors: {
+      type: Object,
+      default: undefined
+    },
+    processing: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    alert: {
+      type: String,
+      default: null
+    },
+    notice: {
+      type: String,
+      default: null
+    }
+  },
+  data () {
+    return {
+      thumbnail: null,
+      title: '',
+      content: '',
+      selectedCategories: []
+    }
+  },
+  computed: {
+    url () {
+      if (!this.thumbnail) { return }
+      return URL.createObjectURL(this.thumbnail)
+    }
+  },
+  methods: {
+    onArticleCreate () {
+      const articleInfo = {
+        thumbnail: this.thumbnail,
+        title: this.title,
+        content: this.content,
+        selectedCategories: this.selectedCategories
+      }
+      this.$emit('article-create', articleInfo)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.post-btn {
+  position: fixed;
+  bottom: 5vh;
+  right: 5vw;
+}
+</style>
--- front/components/templates/ArticleNewTemplate.vue
+++ front/components/templates/ArticleNewTemplate.vue
@@ -2,42 +2,11 @@
   <OneColumnContainer class="pos-relative" fluid>
     <Loading v-if="loading" />
     <Message v-if="!loading" :alert="alert" :notice="notice" />
-    <ValidationObserver v-slot="{ invalid }" ref="observer">
-      <Processing v-if="processing" />
-      <v-form autocomplete="off">
-        <v-row justify="center">
-          <v-col class="py-0 mr-sm-4" cols="12" sm="8" md="8">
-            <ArticleThumbnailFileInput
-              v-model="thumbnail"
-              :url="url"
-            />
-          </v-col>
-          <v-col class="py-0 mr-sm-4" cols="12" sm="8" md="8">
-            <ArticleTitleTextField
-              v-model="title"
-            />
-          </v-col>
-          <v-col class="mb-16" cols="12" sm="8" md="8">
-            <ArticleCategoryCheckbox v-model="selectedCategories" />
-          </v-col>
-          <v-col class="py-0 mr-sm-4" cols="12" sm="8" md="8">
-            <Editor
-              v-model="content"
-            />
-          </v-col>
-          <div class="text-right">
-            <OrangeBtn
-              id="article_create_btn"
-              class="post-btn"
-              :disabled="invalid || processing"
-              @click="onArticleCreate"
-            >
-              作成
-            </OrangeBtn>
-          </div>
-        </v-row>
-      </v-form>
-    </ValidationObserver>
+    <ArticleNewForm
+      v-if="!loading"
+      :processing="processing"
+      @article-create="onArticleCreate"
+    />
   </OneColumnContainer>
 </template>
 
@@ -65,38 +34,10 @@ export default {
       default: null
     }
   },
-  data () {
-    return {
-      thumbnail: null,
-      title: '',
-      content: '',
-      selectedCategories: []
-    }
-  },
-  computed: {
-    url () {
-      if (!this.thumbnail) { return }
-      return URL.createObjectURL(this.thumbnail)
-    }
-  },
   methods: {
-    onArticleCreate () {
-      const articleInfo = {
-        thumbnail: this.thumbnail,
-        title: this.title,
-        content: this.content,
-        selectedCategories: this.selectedCategories
-      }
-      this.$emit('article-create', articleInfo)
+    onArticleCreate (value) {
+      this.$emit('article-create', value)
     }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.post-btn {
-  position: fixed;
-  bottom: 5vh;
-  right: 5vw;
-}
-</style>
