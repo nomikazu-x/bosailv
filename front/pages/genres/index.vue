6290c95493a2010712f691942868fbe0e1483797
kazuki-ayimon <nomikazyu@gmail.com>
2022-06-19 08:54:58 +0900

    feat: ジャンル一覧ページを作成 #55

---
+++ front/pages/genres/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <GenreIndexTemplate
+    :genres="genres"
+    :processing="processing"
+    :loading="loading"
+    :alert="alert"
+    :notice="notice"
+  />
+</template>
+
+<script>
+import Application from '~/plugins/application.js'
+
+export default {
+  name: 'GenresIndex',
+  mixins: [Application],
+  data () {
+    return {
+      genres: null
+    }
+  },
+  async created () {
+    await this.$axios.get(this.$config.apiBaseURL + this.$config.genresUrl)
+      .then((response) => {
+        if (response.data == null) {
+          this.$toasted.error(this.$t('system.error'))
+          this.articles = null
+        } else {
+          this.genres = response.data.genres
+        }
+      },
+      (error) => {
+        this.$toasted.error(this.$t(error.response == null ? 'network.failure' : 'network.error'))
+      })
+
+    this.loading = false
+    this.processing = false
+  }
+}
+</script>
