5d04d97d60b77a75f4ce6db6da1bda8a60630587
kazuki-ayimon <nomikazyu@gmail.com>
2022-06-26 22:56:26 +0900

    test: routing/api/v1/auth/registrations_routing_specを追加 #60

---
+++ api/spec/routing/api/v1/auth/registrations_routing_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::Auth::RegistrationsController, type: :routing do
+  describe 'routing' do
+    it 'routes to #create' do
+      expect(post: '/api/v1/auth/sign_up.json').to route_to('api/v1/auth/registrations#create', format: 'json')
+    end
+    it 'routes to #update' do
+      expect(post: '/api/v1/auth/update.json').to route_to('api/v1/auth/registrations#update', format: 'json')
+    end
+    it 'routes to #image_update' do
+      expect(post: '/api/v1/auth/image/update.json').to route_to('api/v1/auth/registrations#image_update', format: 'json')
+    end
+    it 'routes to #image_destroy' do
+      expect(post: '/api/v1/auth/image/delete.json').to route_to('api/v1/auth/registrations#image_destroy', format: 'json')
+    end
+    it 'routes to #destroy' do
+      expect(post: '/api/v1/auth/delete.json').to route_to('api/v1/auth/registrations#destroy', format: 'json')
+    end
+  end
+end
