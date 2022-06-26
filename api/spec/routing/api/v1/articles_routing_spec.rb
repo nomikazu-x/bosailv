b86c71421efadbb4c204eb88777d8ab9d6ddb3bb
kazuki-ayimon <nomikazyu@gmail.com>
2022-06-26 23:16:50 +0900

    test: spec//routing/api/v1/articles_routing_specを追加 #60

---
+++ api/spec/routing/api/v1/articles_routing_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ArticlesController, type: :routing do
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/api/v1/articles.json').to route_to('api/v1/articles#index', format: 'json')
+    end
+    it 'routes to #create' do
+      expect(post: '/api/v1/articles/create.json').to route_to('api/v1/articles#create', format: 'json')
+    end
+    it 'routes to #show' do
+      expect(get: '/api/v1/articles/1.json').to route_to('api/v1/articles#show', id: '1', format: 'json')
+    end
+    it 'routes to #update' do
+      expect(post: '/api/v1/articles/1/update.json').to route_to('api/v1/articles#update', id: '1', format: 'json')
+    end
+    it 'routes to #destroy' do
+      expect(post: '/api/v1/articles/1/delete.json').to route_to('api/v1/articles#destroy', id: '1', format: 'json')
+    end
+    it 'routes to #search' do
+      expect(get: '/api/v1/articles/search.json').to route_to('api/v1/articles#search', format: 'json')
+    end
+  end
+end
