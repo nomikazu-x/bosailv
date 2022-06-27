814875c7ca55600bf3e431958fd20242791507c6
kazuki-ayimon <nomikazyu@gmail.com>
2022-06-27 18:34:39 +0900

    test: request specを修正 #61

--- api/spec/requests/api/v1/users_spec.rb
+++ api/spec/requests/api/v1/users_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :request do
-  describe "GET /api/v1/users/:username" do
+  describe 'GET /api/v1/users/:username' do
     subject(:call_api){ get "/api/v1/users/#{user.username}.json" }
 
     let(:user) { create(:user) }
 
-    it "ユーザーのデータを返す" do
+    it 'ユーザーのデータを返す' do
       call_api
       res = JSON.parse(response.body)
-      expect(res["user"]["id"]).to eq(User.last.id)
-      expect(res["user"]["name"]).to eq(User.last.name)
+      expect(res['user']['id']).to eq(User.last.id)
+      expect(res['user']['name']).to eq(User.last.name)
       expect(response.status).to eq 200
     end
 
-    context "userが存在しない場合" do
+    context 'userが存在しない場合' do
       it 'レスポンスステータスが404で返る' do
-        get "/api/v1/users/daredesuka.json"
+        get '/api/v1/users/daredesuka.json'
         expect(response.status).to eq 404
       end
     end
