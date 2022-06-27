cc5895682eb1f545ef1c354364be42aa67c4a59b
kazuki-ayimon <nomikazyu@gmail.com>
2022-06-27 19:09:01 +0900

    test: request/users_specを追加 #60

--- api/spec/requests/api/v1/users_spec.rb
+++ api/spec/requests/api/v1/users_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :request do
+  describe 'GET /api/v1/users' do
+    subject(:call_api){ get '/api/v1/users.json' }
+
+    let(:users) { create_list(:user, 5) }
+
+    it 'ユーザー一覧のデータを返す' do
+      call_api
+      res = JSON.parse(response.body)
+      expect(res['users'].length).to eq(User.all.length)
+      expect(response.status).to eq 200
+    end
+  end
+
   describe 'GET /api/v1/users/:username' do
     subject(:call_api){ get "/api/v1/users/#{user.username}.json" }
 
@@ -19,6 +32,20 @@ RSpec.describe Api::V1::UsersController, type: :request do
         get '/api/v1/users/daredesuka.json'
         expect(response.status).to eq 404
       end
+    end
+  end
+
+  describe 'GET /api/v1/users/:username/genres/:id' do
+    subject(:call_api){ get "/api/v1/users/#{user.username}/genres/#{genre.id}.json" }
+
+    let(:user) { create(:user) }
+    let(:genre) { create(:genre) }
+
+    it 'ジャンルのデータを返す' do
+      call_api
+      res = JSON.parse(response.body)
+      expect(res['genre']['id']).to eq(Genre.last.id)
+      expect(response.status).to eq 200
     end
   end
 end
