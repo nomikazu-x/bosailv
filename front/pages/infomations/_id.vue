1555f8be9e78383f3784af15559b0f16310d812e
kazuki-ayimon <nomikazyu@gmail.com>
2022-07-03 10:16:17 +0900

    refactor: インフォメーション関係のページをリファクタリング #63

--- front/pages/infomations/_id.vue
+++ front/pages/infomations/_id.vue
@@ -10,35 +10,8 @@
       <TheMessage v-if="!loading" :alert="alert" :notice="notice" />
     </template>
 
-    <template #left>
-      <v-row v-if="!loading" justify="center">
-        <v-col cols="12">
-          <BaseTitleCard v-if="!loading" title="お知らせ">
-            <div class="pa-3">
-              <v-card-title v-if="infomation">
-                <InfomationLabel :infomation="infomation" />
-                <span class="ml-1 font-weight-bold">
-                  {{ infomation.title }}
-                </span>
-                <span class="ml-1">
-                  <v-icon>mdi-calendar-range</v-icon>
-                  {{ $dateFormat(infomation.started_at, 'ja') }}
-                </span>
-              </v-card-title>
-              <v-card-text v-if="infomation">
-                <!-- eslint-disable-next-line vue/no-v-html -->
-                <div v-if="infomation.body" class="mx-2 my-2" v-html="infomation.body" />
-                <!-- eslint-disable-next-line vue/no-v-html -->
-                <div v-else-if="infomation.summary" class="mx-2 my-2" v-html="infomation.summary" />
-              </v-card-text>
-              <v-divider />
-              <v-card-actions>
-                <NuxtLink to="/infomations" class="ml-2 text-decoration-none">一覧へ</NuxtLink>
-              </v-card-actions>
-            </div>
-          </BaseTitleCard>
-        </v-col>
-      </v-row>
+    <template v-if="!loading" #left>
+      <InfomationShowCard />
     </template>
 
     <template v-if="!loading" #right>
@@ -54,37 +27,7 @@ export default {
   name: 'InfomationsId',
   mixins: [Application],
 
-  data () {
-    return {
-      infomation: null
-    }
-  },
-
-  async created () {
-    await this.$axios.get(this.$config.apiBaseURL + this.$config.infomationDetailUrl.replace('_id', this.$route.params.id))
-      .then((response) => {
-        if (response.data == null) {
-          this.$toasted.error(this.$t('system.error'))
-          return this.$router.push({ path: '/' })
-        }
-        this.infomation = response.data.infomation
-      },
-      (error) => {
-        if (error.response == null) {
-          this.$toasted.error(this.$t('network.failure'))
-          return this.$router.push({ path: '/' })
-        } else if (error.response.data == null && error.response.status !== 404) {
-          this.$toasted.error(this.$t('network.error'))
-          return this.$router.push({ path: '/' })
-        } else {
-          if (error.response.data != null) {
-            this.$toasted.error(error.response.data.alert)
-            this.$toasted.info(error.response.data.notice)
-          }
-          return this.$nuxt.error({ statusCode: error.response.status })
-        }
-      })
-
+  created () {
     this.loading = false
   }
 }
--- front/pages/infomations/index.vue
+++ front/pages/infomations/index.vue
@@ -10,17 +10,8 @@
       <TheMessage v-if="!loading" :alert="alert" :notice="notice" />
     </template>
 
-    <template #left>
-      <v-row v-if="!loading" justify="center">
-        <v-col cols="12">
-          <InfomationCard
-            :info="info"
-            :infomations="infomations"
-            :processing="processing"
-            @pagination="onPagination"
-          />
-        </v-col>
-      </v-row>
+    <template v-if="!loading" #left>
+      <InfomationIndexCard />
     </template>
 
     <template v-if="!loading" #right>
@@ -36,52 +27,8 @@ export default {
   name: 'InfomationsIndex',
   mixins: [Application],
 
-  data () {
-    return {
-      page: 1,
-      info: null,
-      infomations: null
-    }
-  },
-
-  async created () {
-    await this.onPagination(this.page)
-
-    if (!this.$auth.loggedIn) {
-      return this.redirectAuth()
-    }
-
+  created () {
     this.loading = false
-  },
-
-  methods: {
-    async onPagination (page) {
-      this.processing = true
-
-      await this.$axios.get(this.$config.apiBaseURL + this.$config.infomationsUrl, { params: { page } })
-        .then((response) => {
-          if (response.data == null || response.data.infomation == null) {
-            this.$toasted.error(this.$t('system.error'))
-            if (this.info == null) {
-              return this.$router.push({ path: '/' })
-            }
-            this.page = this.info.current_page
-          } else {
-            this.info = response.data.infomation
-            this.infomations = response.data.infomations
-            if (this.$auth.loggedIn && this.$auth.user.infomation_unread_count !== 0 && this.page === 1) { this.$auth.fetchUser() } // Tips: お知らせ未読数をリセット
-          }
-        },
-        (error) => {
-          this.$toasted.error(this.$t(error.response == null ? 'network.failure' : 'network.error'))
-          if (this.info == null) {
-            return this.$router.push({ path: '/' })
-          }
-          this.page = this.info.current_page
-        })
-
-      this.processing = false
-    }
   }
 }
 </script>
