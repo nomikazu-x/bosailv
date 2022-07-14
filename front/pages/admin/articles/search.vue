4da8d29908c35d4076a377c7289eec5fbb378348
kazuki-ayimon <nomikazyu@gmail.com>
2022-07-14 23:11:23 +0900

    feat: 管理者専用ページのメニューを追加 #32

--- front/pages/admin/articles/search.vue
+++ front/pages/admin/articles/search.vue
@@ -1,11 +1,42 @@
 <template>
-  <p>admin only</p>
+  <TwoColumnContainer
+    :left-cols="12"
+    :left-sm="4"
+    :right-cols="12"
+    :right-sm="8"
+  >
+    <template #top>
+      <TheLoading v-if="loading" />
+      <TheMessage v-if="!loading" :alert="alert" :notice="notice" />
+    </template>
+
+    <template v-if="!loading" #left>
+      <AdminMenusCard />
+    </template>
+
+    <template v-if="!loading" #right>
+      <p>admin only</p>
+    </template>
+  </TwoColumnContainer>
 </template>
 
 <script>
 import Application from '~/plugins/application.js'
+import TwoColumnContainer from '~/components/molecules/containers/TwoColumnContainer.vue'
+import TheLoading from '~/components/organisms/application/TheLoading.vue'
+import TheMessage from '~/components/organisms/application/TheMessage.vue'
+import AdminMenusCard from '~/components/organisms/cards/admin/AdminMenusCard.vue'
 
 export default {
+  name: 'AdminArticlesSearch',
+
+  components: {
+    TwoColumnContainer,
+    TheLoading,
+    TheMessage,
+    AdminMenusCard
+  },
+
   mixins: [Application],
 
   created () {
