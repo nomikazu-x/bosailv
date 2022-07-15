3b776efbe636fc0bfcc6843bd3d3017f235a262d
kazuki-ayimon <nomikazyu@gmail.com>
2022-07-15 21:02:57 +0900

    fix: 削除確認をコンポーネントに分けた #32

---
+++ front/components/organisms/dialogs/DeleteConfirmDialog.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card-text>
+    <v-dialog transition="dialog-top-transition" max-width="600px">
+      <template #activator="{ on, attrs }">
+        <v-btn color="error" v-bind="attrs" v-on="on">削除</v-btn>
+      </template>
+      <template #default="dialog">
+        <v-card>
+          <v-toolbar color="error" dark>削除</v-toolbar>
+          <v-card-text>
+            <div class="text-h6 pa-6">本当に削除しますか？</div>
+          </v-card-text>
+          <v-card-actions class="justify-end">
+            <v-btn color="secondary" @click="dialog.value = false">いいえ</v-btn>
+            <v-btn color="error" @click="dialog.value = false; onClick()">はい</v-btn>
+          </v-card-actions>
+        </v-card>
+      </template>
+    </v-dialog>
+  </v-card-text>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClick () {
+      this.$emit('click')
+    }
+  }
+}
+</script>
--- front/components/organisms/items/UserListItem.vue
+++ front/components/organisms/items/UserListItem.vue
@@ -11,25 +11,7 @@
       </BaseListItem>
     </v-col>
     <v-col cols="2">
-      <v-card-text>
-        <v-dialog transition="dialog-top-transition" max-width="600px">
-          <template #activator="{ on, attrs }">
-            <v-btn id="user_delete_btn" color="error" :disabled="processing" v-bind="attrs" v-on="on">削除</v-btn>
-          </template>
-          <template #default="dialog">
-            <v-card id="user_delete_dialog">
-              <v-toolbar color="error" dark>アカウント削除</v-toolbar>
-              <v-card-text>
-                <div class="text-h6 pa-6">本当に削除しますか？</div>
-              </v-card-text>
-              <v-card-actions class="justify-end">
-                <v-btn id="user_delete_no_btn" color="secondary" @click="dialog.value = false">いいえ</v-btn>
-                <v-btn id="user_delete_yes_btn" color="error" @click="dialog.value = false; onUserDelete(user.id)">はい</v-btn>
-              </v-card-actions>
-            </v-card>
-          </template>
-        </v-dialog>
-      </v-card-text>
+      <DeleteConfirmDialog @click="onUserDelete(user.id)" />
     </v-col>
     <v-divider />
   </v-row>
@@ -38,12 +20,14 @@
 <script>
 import Application from '~/plugins/application.js'
 import BaseListItem from '~/components/molecules/items/BaseListItem.vue'
+import DeleteConfirmDialog from '~/components/organisms/dialogs/DeleteConfirmDialog.vue'
 
 export default {
   name: 'UserListItem',
 
   components: {
-    BaseListItem
+    BaseListItem,
+    DeleteConfirmDialog
   },
 
   mixins: [Application],
