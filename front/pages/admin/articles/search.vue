1b28d6a3864a4c72e77db716dcca88b8e5a7a6ee
kazuki-ayimon <nomikazyu@gmail.com>
2022-07-16 00:14:53 +0900

    feat: 管理者ページの記事検索ができるようにした #32

--- front/pages/admin/articles/search.vue
+++ front/pages/admin/articles/search.vue
@@ -15,7 +15,7 @@
     </template>
 
     <template v-if="!loading" #right>
-      <p>admin only</p>
+      <ArticleSearchListCard />
     </template>
   </TwoColumnContainer>
 </template>
@@ -26,6 +26,7 @@ import TwoColumnContainer from '~/components/molecules/containers/TwoColumnConta
 import TheLoading from '~/components/organisms/application/TheLoading.vue'
 import TheMessage from '~/components/organisms/application/TheMessage.vue'
 import AdminMenusCard from '~/components/organisms/cards/admin/AdminMenusCard.vue'
+import ArticleSearchListCard from '~/components/organisms/cards/article/ArticleSearchListCard.vue'
 
 export default {
   name: 'AdminArticlesSearch',
@@ -34,7 +35,8 @@ export default {
     TwoColumnContainer,
     TheLoading,
     TheMessage,
-    AdminMenusCard
+    AdminMenusCard,
+    ArticleSearchListCard
   },
 
   mixins: [Application],
