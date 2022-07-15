859315706c6e924a50d64b20a50f344bb6012976
kazuki-ayimon <nomikazyu@gmail.com>
2022-07-16 01:03:03 +0900

    feat: 管理者ページのジャンル一覧を取得できるようにした。あわせて、編集ページに遷移できるようにした #32

--- front/pages/admin/genres/index.vue
+++ front/pages/admin/genres/index.vue
@@ -15,7 +15,7 @@
     </template>
 
     <template v-if="!loading" #right>
-      <p>admin only</p>
+      <GenreImageListCardWithTitle />
     </template>
   </TwoColumnContainer>
 </template>
@@ -26,6 +26,7 @@ import TwoColumnContainer from '~/components/molecules/containers/TwoColumnConta
 import TheLoading from '~/components/organisms/application/TheLoading.vue'
 import TheMessage from '~/components/organisms/application/TheMessage.vue'
 import AdminMenusCard from '~/components/organisms/cards/admin/AdminMenusCard.vue'
+import GenreImageListCardWithTitle from '~/components/organisms/cards/genre/GenreImageListCardWithTitle.vue'
 
 export default {
   name: 'AdminGenres',
@@ -34,7 +35,8 @@ export default {
     TwoColumnContainer,
     TheLoading,
     TheMessage,
-    AdminMenusCard
+    AdminMenusCard,
+    GenreImageListCardWithTitle
   },
 
   mixins: [Application],
