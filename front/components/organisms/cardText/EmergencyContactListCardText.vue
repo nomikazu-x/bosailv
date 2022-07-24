877c7ae282b4e4ef67457b879450dcd61d32e621
kazuki-ayimon <nomikazyu@gmail.com>
2022-07-24 10:53:46 +0900

    feat: 緊急連絡先をリアクティブに作成・削除できるようにした。また、バリデーションも追加 #76

---
+++ front/components/organisms/cardText/EmergencyContactListCardText.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-card-text>
+    <TheProcessing v-if="processing" />
+    <v-row>
+      <v-col cols="12">
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <v-card-title>{{ emergencyContact.name }}</v-card-title>
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <v-card-text>
+          <a class="text-decoration-none" :href="`tel:${emergencyContact.phone_number}`">
+            {{ emergencyContact.phone_number }}
+          </a>
+        </v-card-text>
+        <div class="text-right">
+          <v-btn icon>
+            <v-icon size="20" @click="onEmergencyContactDelete(emergencyContact.id)">
+              mdi-trash-can-outline
+            </v-icon>
+          </v-btn>
+        </div>
+        <v-divider class="my-4" />
+      </v-col>
+    </v-row>
+  </v-card-text>
+</template>
+
+<script>
+import Application from '~/plugins/application.js'
+
+export default {
+  name: 'EmergencyContactListCardText',
+
+  mixins: [Application],
+
+  props: {
+    emergencyContact: {
+      type: Object,
+      default: null
+    }
+  },
+
+  created () {
+    this.processing = false
+  },
+
+  methods: {
+    async onEmergencyContactDelete (emergencyContactId) {
+      this.processing = true
+
+      await this.$axios.post(this.$config.apiBaseURL + this.$config.emergencyContactDeleteUrl.replace('_id', emergencyContactId))
+        .then((response) => {
+          if (response.data == null) {
+            this.$toasted.error(this.$t('system.error'))
+          } else {
+            this.$store.commit('emergencyContacts/deleteEmergencyContact', emergencyContactId, { root: true })
+            this.$toasted.error(response.data.alert)
+            this.$toasted.info(response.data.notice)
+          }
+        },
+        (error) => {
+          if (error.response == null) {
+            this.$toasted.error(this.$t('network.failure'))
+          } else if (error.response.data == null) {
+            this.$toasted.error(this.$t('network.error'))
+          } else {
+            this.$toasted.error(error.response.data.alert)
+            this.$toasted.info(error.response.data.notice)
+          }
+        })
+
+      this.processing = false
+    }
+  }
+}
+</script>
---
+++ front/components/organisms/form/EmergencyContactForm.vue
@@ -0,0 +1,97 @@
+<template>
+  <ValidationObserver v-slot="{ invalid }" ref="observer">
+    <TheProcessing v-if="processing" />
+    <v-form autocomplete="off">
+      <v-card-text>
+        <BaseTextField
+          v-model="name"
+          label="連絡先名"
+          rules="required"
+          @click="waiting = false"
+        />
+        <BaseTextField
+          v-model="phoneNumber"
+          label="電話番号"
+          :rules="{ required: true, regex: /^0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}$|^0[5789]0[-]?\d{4}[-]?\d{4}$/ }"
+          @click="waiting = false"
+        />
+        <RedBtn id="emergency_contact_create_btn" :disabled="invalid || processing || waiting" @click="onEmergencyContactCreate">作成</RedBtn>
+      </v-card-text>
+    </v-form>
+  </ValidationObserver>
+</template>
+
+<script>
+import { ValidationObserver, extend, configure, localize } from 'vee-validate'
+import { required, regex } from 'vee-validate/dist/rules'
+import Application from '~/plugins/application.js'
+import BaseTextField from '~/components/molecules/textFields/BaseTextField.vue'
+import RedBtn from '~/components/atoms/btns/RedBtn.vue'
+
+extend('required', required)
+extend('regex', regex)
+configure({ generateMessage: localize('ja', require('~/locales/validate.ja.js')) })
+
+export default {
+  name: 'EmergencyContactForm',
+
+  components: {
+    ValidationObserver,
+    BaseTextField,
+    RedBtn
+  },
+
+  mixins: [Application],
+
+  data () {
+    return {
+      waiting: false,
+      name: '',
+      phoneNumber: ''
+    }
+  },
+
+  created () {
+    this.processing = false
+  },
+
+  methods: {
+    async onEmergencyContactCreate () {
+      this.processing = true
+
+      await this.$axios.post(this.$config.apiBaseURL + this.$config.emergencyContactCreateUrl, {
+        name: this.name,
+        phone_number: this.phoneNumber
+      })
+        .then((response) => {
+          if (response.data == null) {
+            this.$toasted.error(this.$t('system.error'))
+          } else {
+            this.$store.commit('emergencyContacts/addEmergencyContacts', response.data.emergency_contact, { root: true })
+            this.name = ''
+            this.phoneNumber = ''
+            this.$refs.observer.reset()
+            this.$toasted.error(response.data.alert)
+            this.$toasted.info(response.data.notice)
+          }
+        },
+        (error) => {
+          if (error.response == null) {
+            this.$toasted.error(this.$t('network.failure'))
+          } else if (error.response.data == null) {
+            this.$toasted.error(this.$t('network.error'))
+          } else {
+            this.$emit('alert', error.response.data.alert)
+            this.$emit('notice', error.response.data.notice)
+            if (error.response.data.errors != null) {
+              this.$refs.observer.setErrors(error.response.data.errors)
+              this.waiting = true
+            }
+          }
+        })
+
+      this.processing = false
+    }
+  }
+}
+</script>
