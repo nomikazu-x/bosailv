b3bcd79e480e44bed1301824e070b4e55ff5b228
kazuki-ayimon <nomikazyu@gmail.com>
2022-07-26 10:09:46 +0900

    feat: 備蓄完了・未完了ボタンを配置 #78

--- front/components/organisms/cards/task/StockListCard.vue
+++ front/components/organisms/cards/task/StockListCard.vue
@@ -2,32 +2,27 @@
   <BaseTitleCard v-if="stocks !== null" title="備蓄品リスト">
     <TheProcessing v-if="processing" />
     <v-card v-for="stock in stocks" :key="stock.id" class="my-3" tile>
-      <v-row align="center">
-        <v-col cols="6">
+      <v-row v-if="stock.count !== 0" align="center">
+        <v-col cols="7">
           <v-card-subtitle>
             {{ stock.name }}
           </v-card-subtitle>
         </v-col>
-        <v-col cols="3">
+        <v-col cols="2">
           <v-card-subtitle>
             {{ stock.count }}
           </v-card-subtitle>
         </v-col>
         <v-col cols="3">
-          <v-card-subtitle>
-            {{ stock.name }}
-          </v-card-subtitle>
-        </v-col>
-        <!-- <v-col cols="4">
           <div class="text-center">
             <CompleteBtnGroup
               v-if="$auth.loggedIn"
-              :task="task"
-              @complete="onComplete(task.value)"
-              @un-complete="onUnComplete(task.value)"
+              :task="stock"
+              @complete="onComplete(stock.value)"
+              @un-complete="onUnComplete(stock.value)"
             />
           </div>
-        </v-col> -->
+        </v-col>
       </v-row>
     </v-card>
   </BaseTitleCard>
@@ -36,13 +31,14 @@
 <script>
 import Application from '~/plugins/application.js'
 import BaseTitleCard from '~/components/molecules/cards/BaseTitleCard.vue'
-// import CompleteBtnGroup from '~/components/organisms/btnGroup/CompleteBtnGroup.vue'
+import CompleteBtnGroup from '~/components/organisms/btnGroup/CompleteBtnGroup.vue'
 
 export default {
   name: 'StockListCard',
 
   components: {
-    BaseTitleCard
+    BaseTitleCard,
+    CompleteBtnGroup
   },
 
   mixins: [Application],
@@ -68,6 +64,74 @@ export default {
         return this.$router.push({ path: '/' })
       })
     this.processing = false
+  },
+
+  methods: {
+    async onComplete (task) {
+      this.processing = true
+
+      await this.$axios.post(this.$config.apiBaseURL + this.$config.taskProfileUpdateUrl, {
+        stock_task: task
+      })
+        .then((response) => {
+          if (response.data == null) {
+            this.$toasted.error(this.$t('system.error'))
+          } else if (this.$auth.loggedIn) {
+            this.$store.commit('user/setPoint', response.data.user, { root: true })
+            this.$store.commit('user/setRequiredPoint', response.data.required_point, { root: true })
+            this.$toasted.info(response.data.notice)
+          } else {
+            return this.redirectSignIn(response.data.alert, response.data.notice)
+          }
+        },
+        (error) => {
+          if (error.response == null) {
+            this.$toasted.error(this.$t('network.failure'))
+          } else if (error.response.status === 401) {
+            return this.signOut()
+          } else if (error.response.data == null) {
+            this.$toasted.error(this.$t('network.error'))
+          } else {
+            this.$emit('alert', error.response.data.alert)
+            this.$emit('notice', error.response.data.notice)
+          }
+        })
+
+      this.processing = false
+    },
+
+    async onUnComplete (task) {
+      this.processing = true
+
+      await this.$axios.post(this.$config.apiBaseURL + this.$config.taskProfileDeleteUrl, {
+        stock_task: task
+      })
+        .then((response) => {
+          if (response.data == null) {
+            this.$toasted.error(this.$t('system.error'))
+          } else if (this.$auth.loggedIn) {
+            this.$store.commit('user/setPoint', response.data.user, { root: true })
+            this.$store.commit('user/setRequiredPoint', response.data.required_point, { root: true })
+            this.$toasted.info(response.data.notice)
+          } else {
+            return this.redirectSignIn(response.data.alert, response.data.notice)
+          }
+        },
+        (error) => {
+          if (error.response == null) {
+            this.$toasted.error(this.$t('network.failure'))
+          } else if (error.response.status === 401) {
+            return this.signOut()
+          } else if (error.response.data == null) {
+            this.$toasted.error(this.$t('network.error'))
+          } else {
+            this.$emit('alert', error.response.data.alert)
+            this.$emit('notice', error.response.data.notice)
+          }
+        })
+
+      this.processing = false
+    }
   }
 }
 </script>
