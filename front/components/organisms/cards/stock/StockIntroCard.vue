c0852af6146665f24f07aebe40792039702c8c98
kazuki-ayimon <nomikazyu@gmail.com>
2022-09-13 21:05:57 +0900

    fix: 備蓄品リストの紹介ページを修正

--- front/components/organisms/cards/stock/StockIntroCard.vue
+++ front/components/organisms/cards/stock/StockIntroCard.vue
@@ -10,6 +10,48 @@
           電気やガス、水道などのライフラインが止まった場合や、避難生活が必要になったときのために防災用品を備えましょう。
         </v-card-text>
       </v-col>
+      <v-col cols="12">
+        <v-card outlined tile color="#DFEEE5">
+          <v-card-text>
+            <div class="main-heading font-weight-bold" style="color: #117768;">
+              水・食料品
+            </div>
+            水：飲料水（1人1日3リットル）、給水タンク、給水袋<br>
+            食料品：アルファ化米、レトルトご飯、缶詰（さばの味噌煮、野菜など）、レトルト食品（冷凍食品）、缶詰（果物、小豆など）、加熱無しで食べられる食品（かまぼこ、チーズなど）、栄養補助食品
+          </v-card-text>
+          <v-card-text>
+            <div class="main-heading font-weight-bold" style="color: #117768;">
+              調理補助品
+            </div>
+            カセットコンロ、カセットボンベ、マルチツール（缶切り・ナイフなど）
+          </v-card-text>
+          <v-card-text>
+            <div class="main-heading font-weight-bold" style="color: #117768;">
+              薬・衛生用品
+            </div>
+            薬：常備薬・市販薬、救急箱<br>
+            衛生用品：簡易トイレ、トイレットペーパー、ティッシュペーパー、大型ビニール袋・ゴミ袋、除菌ウェットティッシュ
+          </v-card-text>
+          <v-card-text>
+            <div class="main-heading font-weight-bold" style="color: #117768;">
+              日用品
+            </div>
+            懐中電灯、乾電池、ライター、点火棒、マッチ、ロープ・ガムテープ、軍手、携帯電話・スマートフォンの予備バッテリー、手回し充電式などのラジオ、使い捨てコンタクトレンズ、生理用品、化粧品
+          </v-card-text>
+          <v-card-text>
+            <div class="main-heading font-weight-bold" style="color: #117768;">
+              乳幼児向け用品
+            </div>
+            スティックタイプの粉ミルク、離乳食（アレルギー対応）、おむつ、お尻拭き
+          </v-card-text>
+          <v-card-text>
+            <div class="main-heading font-weight-bold" style="color: #117768;">
+              高齢者向け用品
+            </div>
+            おかゆなどのやわらかい食品、高齢者用食品、入れ歯洗浄剤、常備薬（処方薬）、老眼鏡、お薬手帳、補聴器・予備の電池
+          </v-card-text>
+        </v-card>
+      </v-col>
       <v-col cols="12" class="mt-5">
         <div class="main-heading font-weight-bold" style=" color: #117768;">
           <v-icon class="mr-2" color="#117768">mdi-checkbox-marked-circle</v-icon>
