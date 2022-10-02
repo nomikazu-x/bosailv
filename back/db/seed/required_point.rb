bf69debc686f3379b9f6e05708667e7fa5e0f2e6
kazuki-ayimon <nomikazyu@gmail.com>
2022-10-02 18:24:27 +0900

    test: buildspec.ymlのテスト

--- back/db/seed/required_point.rb
+++ back/db/seed/required_point.rb
@@ -1,17 +1,15 @@
 #次のレベルに必要な経験値算出
 sum = 0
-pre = 0
 100.times do |i|
   i += 1
   if i == 1
     point = 5
   else
-    point = 5 + i
+    point = 4 + i
   end
   point = point.round
   sum += point
   puts "#{i}レベル 次のレベルに必要な経験値: #{point} 累計: #{sum}"
-  pre = point
 
   RequiredPoint.create!(level: i, point: point, lifelong_point: sum)
 end
