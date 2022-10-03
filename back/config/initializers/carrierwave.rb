404b45c054c4e7554b121711f654a1ba2fec2890
kazuki-ayimon <nomikazyu@gmail.com>
2022-10-03 21:34:53 +0900

    feat: carrierwave,s3の本番用設定

---
+++ back/config/initializers/carrierwave.rb
@@ -0,0 +1,19 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.cache_storage = :fog
+    config.fog_directory = 'kb-s3-bucket-for-images'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+      region: 'ap-northeast-1'
+    }
+  else
+    config.asset_host = 'http://localhost:8081'
+    config.cache_storage = :file
+  end
+end
