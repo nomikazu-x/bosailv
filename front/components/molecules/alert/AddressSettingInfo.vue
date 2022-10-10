2790db354d77cd7c4b83d7faa1088de9bb29b80c
kazuki-ayimon <nomikazyu@gmail.com>
2022-10-10 16:34:57 +0900

    fix: アカウント削除ページにユーザーがわかりやすいテキストを追加

--- front/components/molecules/alert/AddressSettingInfo.vue
+++ front/components/molecules/alert/AddressSettingInfo.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-alert v-if="$auth.loggedIn && $auth.user != null && ($auth.user.prefecture == null && $auth.user.city == null)" type="warning">
-    あなたの住んている市町村・都道府県を登録しておきましょう。
+  <v-alert v-if="$auth.loggedIn && $auth.user != null && ($auth.user.prefecture == null && $auth.user.city == null)" type="success">
+    あなたの住んでいる市町村・都道府県を登録しておきましょう。
     <br>避難所やハザードマップの検索が簡単にできるようになります。
   </v-alert>
 </template>
