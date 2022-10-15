f921f690149aa5723280e3d5205d0fa1913504da
kazuki-ayimon <nomikazyu@gmail.com>
2022-10-15 10:42:39 +0900

    refactor: task_profileをリファクタリング

--- back/app/controllers/api/v1/task_profiles_controller.rb
+++ back/app/controllers/api/v1/task_profiles_controller.rb
@@ -12,12 +12,7 @@ class Api::V1::TaskProfilesController < Api::V1::ApplicationController
     when params[:sns_task]
       task = @task_profile.sns_tasks.set(params[:sns_task])
       if @task_profile.update(params.permit(sns_tasks: task))
-        ActiveRecord::Base.transaction do
-          # ポイント獲得
-          PointRecorder.new(current_user).record(Settings['sns_task_complete_obtained_point'])
-          update_auth_header # 成功時のみ認証情報を返す
-          render './api/v1/task_profiles/success', locals: { notice: I18n.t('notice.user.task_update') }
-        end
+        task_complete_point_record(Settings['sns_task_complete_obtained_point'])
       else
         render './api/v1/failure', locals: { alert: I18n.t('alert.user.task_update') }, status: :unprocessable_entity
       end
@@ -25,12 +20,7 @@ class Api::V1::TaskProfilesController < Api::V1::ApplicationController
     when params[:house_task]
       task = @task_profile.house_tasks.set(params[:house_task])
       if @task_profile.update(params.permit(house_tasks: task))
-        ActiveRecord::Base.transaction do
-          # ポイント獲得
-          PointRecorder.new(current_user).record(Settings['house_task_complete_obtained_point'])
-          update_auth_header # 成功時のみ認証情報を返す
-          render './api/v1/task_profiles/success', locals: { notice: I18n.t('notice.user.task_update') }
-        end
+        task_complete_point_record(Settings['house_task_complete_obtained_point'])
       else
         render './api/v1/failure', locals: { alert: I18n.t('alert.user.task_update') }, status: :unprocessable_entity
       end
@@ -38,12 +28,7 @@ class Api::V1::TaskProfilesController < Api::V1::ApplicationController
     when params[:stock_task]
       task = @task_profile.stock_tasks.set(params[:stock_task])
       if @task_profile.update(params.permit(stock_tasks: task))
-        ActiveRecord::Base.transaction do
-          # ポイント獲得
-          PointRecorder.new(current_user).record(Settings['stock_task_complete_obtained_point'])
-          update_auth_header # 成功時のみ認証情報を返す
-          render './api/v1/task_profiles/success', locals: { notice: I18n.t('notice.user.task_update') }
-        end
+        task_complete_point_record(Settings['stock_task_complete_obtained_point'])
       else
         render './api/v1/failure', locals: { alert: I18n.t('alert.user.task_update') }, status: :unprocessable_entity
       end
@@ -69,12 +54,7 @@ class Api::V1::TaskProfilesController < Api::V1::ApplicationController
     when params[:sns_task]
       task = @task_profile.sns_tasks.unset(params[:sns_task].to_sym) # Tips unsetメソッドの場合はシンボルにする必要がある。
       if @task_profile.update(params.permit(sns_tasks: task))
-        ActiveRecord::Base.transaction do
-          # ポイントを減らす
-          PointRecorder.new(current_user).delete_record(Settings['sns_task_complete_obtained_point'])
-          update_auth_header # 成功時のみ認証情報を返す
-          render './api/v1/task_profiles/success', locals: { notice: I18n.t('notice.user.task_destroy') }
-        end
+        task_complete_point_delete_record(Settings['sns_task_complete_obtained_point'])
       else
         render './api/v1/failure', locals: { alert: I18n.t('alert.user.task_destroy') }, status: :unprocessable_entity
       end
@@ -82,12 +62,7 @@ class Api::V1::TaskProfilesController < Api::V1::ApplicationController
     when params[:house_task]
       task = @task_profile.house_tasks.unset(params[:house_task].to_sym) # Tips unsetメソッドの場合はシンボルにする必要がある。
       if @task_profile.update(params.permit(house_tasks: task))
-        ActiveRecord::Base.transaction do
-          # ポイントを減らす
-          PointRecorder.new(current_user).delete_record(Settings['house_task_complete_obtained_point'])
-          update_auth_header # 成功時のみ認証情報を返す
-          render './api/v1/task_profiles/success', locals: { notice: I18n.t('notice.user.task_destroy') }
-        end
+          task_complete_point_delete_record(Settings['house_task_complete_obtained_point'])
       else
         render './api/v1/failure', locals: { alert: I18n.t('alert.user.task_destroy') }, status: :unprocessable_entity
       end
@@ -95,12 +70,7 @@ class Api::V1::TaskProfilesController < Api::V1::ApplicationController
     when params[:stock_task]
       task = @task_profile.stock_tasks.unset(params[:stock_task].to_sym) # Tips unsetメソッドの場合はシンボルにする必要がある。
       if @task_profile.update(params.permit(stock_tasks: task))
-        ActiveRecord::Base.transaction do
-          # ポイントを減らす
-          PointRecorder.new(current_user).delete_record(Settings['stock_task_complete_obtained_point'])
-          update_auth_header # 成功時のみ認証情報を返す
-          render './api/v1/task_profiles/success', locals: { notice: I18n.t('notice.user.task_destroy') }
-        end
+        task_complete_point_delete_record(Settings['stock_task_complete_obtained_point'])
       else
         render './api/v1/failure', locals: { alert: I18n.t('alert.user.task_destroy') }, status: :unprocessable_entity
       end
@@ -113,5 +83,23 @@ class Api::V1::TaskProfilesController < Api::V1::ApplicationController
 
   def set_task_profile
     @task_profile = current_user.prepare_task_profile
+  end
+
+  def task_complete_point_record(obtained_point)
+    ActiveRecord::Base.transaction do
+      # ポイント獲得
+      PointRecorder.new(current_user).record(obtained_point)
+      update_auth_header # 成功時のみ認証情報を返す
+      render './api/v1/task_profiles/success', locals: { notice: I18n.t('notice.user.task_update') }
+    end
+  end
+
+  def task_complete_point_delete_record(obtained_point)
+    ActiveRecord::Base.transaction do
+      # ポイントを減らす
+      PointRecorder.new(current_user).delete_record(obtained_point)
+      update_auth_header # 成功時のみ認証情報を返す
+      render './api/v1/task_profiles/success', locals: { notice: I18n.t('notice.user.task_destroy') }
+    end
   end
 end
