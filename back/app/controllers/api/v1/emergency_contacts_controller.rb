8ad47259cc4b7d3a50176bc94b61d369bd02cafd
kazuki-ayimon <nomikazyu@gmail.com>
2022-10-27 22:39:42 +0900

    test: emergency_contactのrequest_specを追加 #125

--- back/app/controllers/api/v1/emergency_contacts_controller.rb
+++ back/app/controllers/api/v1/emergency_contacts_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::EmergencyContactsController < Api::V1::ApplicationController
+  before_action :authenticate_user!, only: %i[create destroy]
   before_action :check_count, only: %i[create]
+  before_action :correct_user?, only: %i[destroy]
 
   # GET /api/v1/emergency_contacts(.json) 緊急連絡先一覧API
   def index
@@ -23,9 +25,7 @@ class Api::V1::EmergencyContactsController < Api::V1::ApplicationController
 
   # POST /api/v1/emergency_contacts/:id/delete(.json) 緊急連絡先削除API(処理)
   def destroy
-    emergency_contact = current_user.emergency_contacts.find(params[:id])
-
-    if emergency_contact.destroy
+    if @emergency_contact.destroy
       ActiveRecord::Base.transaction do
         # ポイントを減らす
         PointRecorder.new(current_user).delete_record(Settings['emergency_contact_create_obtained_point'])
@@ -46,5 +46,12 @@ class Api::V1::EmergencyContactsController < Api::V1::ApplicationController
     return if current_user.emergency_contacts.count < Settings['maximum_emergency_contacts_length']
 
     render './api/v1/failure', locals: { alert: I18n.t('alert.emergency_contact.create') }, status: :unprocessable_entity
+  end
+
+  def correct_user?
+    @emergency_contact = EmergencyContact.find(params[:id])
+    return if current_user == @emergency_contact.user
+
+    render './api/v1/failure', locals: { alert: I18n.t('errors.messages.not_permission') }, status: :unauthorized
   end
 end
