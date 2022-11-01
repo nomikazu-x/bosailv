700351ef9415a76cdb784113bc6139631325e808
kazuki-ayimon <nomikazyu@gmail.com>
2022-11-01 22:46:02 +0900

    test: shelter_registrationのrequest specを追加 #125

---
+++ back/spec/requests/api/v1/shelter_registrations_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ShelterRegistrationsController, type: :request do
+  describe 'POST /api/v1/shelters/:id/shelter_registrations/create' do
+    subject(:call_api) { post "/api/v1/shelters/#{shelter.id}/shelter_registrations/create.json", headers: headers, params: params }
+
+    let(:user) { create(:confirmed_user) }
+    let(:user2) { create(:confirmed_user, username: 'user2') }
+    let(:shelter) { create(:shelter)}
+    let(:headers) { user2.create_new_auth_token }
+    let(:params) {{ }}
+
+    context 'お気に入りすることに成功した場合' do
+      let(:params) do
+        {
+          shelter_registration: {
+            user_id: user2.id,
+            shelter_id: shelter.id
+          }
+        }
+      end
+
+      it 'レスポンスステータスが200で返ること' do
+        call_api
+        res = JSON.parse(response.body)
+        expect(res['success']).to eq(true)
+        expect(response.status).to eq 200
+      end
+
+      it 'お気に入りされていること' do
+        expect { call_api }.to change { ShelterRegistration.count }.by(1)
+      end
+
+      it 'レスポンスボディーに期待された値が返ること' do
+        call_api
+        res = JSON.parse(response.body)
+        expect(res['user']['id']).to eq User.last.id
+      end
+    end
+  end
+
+  describe 'POST /api/v1/shelters/:id/shelter_registrations/delete' do
+    subject(:call_api) { post "/api/v1/shelters/#{shelter.id}/shelter_registrations/delete.json", headers: headers }
+
+    let(:user) { create :confirmed_user }
+    let(:user2) { create :confirmed_user, username: 'testname' }
+    let(:headers) { user.create_new_auth_token }
+    let(:shelter) { create :shelter }
+    let!(:shelter_registration) { create :shelter_registration, user_id: user.id, shelter_id: shelter.id }
+    let!(:point_record) { create :point_record, user_id: user.id, obtained_point: Settings['shelter_registration_obtained_point'] }
+
+    context 'お気に入りの削除に成功した場合' do
+      it 'レスポンスステータスが200で返ること' do
+        call_api
+        res = JSON.parse(response.body)
+        expect(res['success']).to eq(true)
+        expect(response.status).to eq 200
+      end
+    end
+  end
+end
