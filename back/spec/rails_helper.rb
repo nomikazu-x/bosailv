6d3e04eee4e899decce864d8ba5dd95df0d72871
kazuki-ayimon <nomikazyu@gmail.com>
2022-11-17 23:58:46 +0900

    fix: rails_helperを修正 #145

--- back/spec/rails_helper.rb
+++ back/spec/rails_helper.rb
@@ -13,6 +13,10 @@ require 'rspec/rails'
 # Use devise
 require 'devise'
 
+# Use Ruby Tests Profiling Toolbox
+require 'test_prof/recipes/rspec/let_it_be'
+require 'test_prof/recipes/rspec/before_all'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
