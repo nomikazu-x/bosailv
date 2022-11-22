dce14e8bcaff9472bb82792dec07ad753be1b650
kazuki-ayimon <nomikazyu@gmail.com>
2022-11-22 22:56:56 +0900

    refactor: routesをリファクタリング

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,29 +1,9 @@
 Rails.application.routes.draw do
+  draw :admin
+  draw :auth
+
   namespace :api do
     namespace :v1 do
-      # [管理者権限]
-      namespace :admin do
-        # ユーザー
-        get  'users',                      to: 'users#index',          as: 'users'
-        get  'users/search',               to: 'users#search',         as: 'search_users'
-        post 'users/:username/delete',     to: 'users#destroy',        as: 'destroy_users'
-
-        # ジャンル
-        post 'genres/create',       to: 'genres#create',       as: 'create_genre'
-        post 'genres/:id/update',   to: 'genres#update',       as: 'update_genre'
-        post 'genres/:id/delete',   to: 'genres#destroy',      as: 'destroy_genre'
-
-        # お知らせ
-        get  'infomations',            to: 'infomations#index',     as: 'infomations'
-        post 'infomations/create',     to: 'infomations#create',    as: 'create_infomation'
-        post 'infomations/:id/delete', to: 'infomations#destroy',   as: 'destroy_infomation'
-
-        # 防災タスク
-        post 'tasks/create',       to: 'tasks#create',       as: 'create_task'
-        post 'tasks/:id/update',   to: 'tasks#update',       as: 'update_task'
-        post 'tasks/:id/delete',   to: 'tasks#destroy',      as: 'destroy_task'
-      end
-
       # ユーザー
       get   'users',                      to: 'users#index',          as: 'users'
       get   'users/:username',            to: 'users#show',           as: 'show_users'
@@ -107,37 +87,6 @@ Rails.application.routes.draw do
     end
     
     get :health_check, to: 'health_check#index'
-  end
-
-  scope :api do
-    scope :v1 do
-      get  'users/confirmation',        to: 'users/confirmations#show',          as: 'user_confirmation' # Tips: メールアドレス確認に必要
-      get  'users/unlock',              to: 'users/unlocks#show',                as: 'user_unlock' # Tips: アンロックに必要
-      get  'users/password',            to: 'users/passwords#edit',              as: 'edit_user_password' # Tips: パスワード変更に必要
-
-      # Devise Token Auth
-      devise_for :users, skip: :all
-      devise_scope :user do
-        post 'auth/sign_up',         to: 'api/v1/auth/registrations#create',             as: 'create_auth_registration'
-        get  'auth/detail',          to: 'api/v1/auth/registrations#show',               as: 'show_auth_registration'
-        post 'auth/update',          to: 'api/v1/auth/registrations#update',             as: 'update_auth_registration'
-        post 'auth/profile/update',  to: 'api/v1/auth/registrations#profile_update',     as: 'update_auth_profile_registration'
-        post 'auth/image/update',    to: 'api/v1/auth/registrations#image_update',       as: 'update_auth_image_registration'
-        post 'auth/image/delete',    to: 'api/v1/auth/registrations#image_destroy',      as: 'delete_auth_image_registration'
-        post 'auth/delete',          to: 'api/v1/auth/registrations#destroy',            as: 'destroy_auth_registration'
-        post 'auth/confirmation',    to: 'api/v1/auth/confirmations#create',             as: 'create_auth_confirmation'
-        get  'auth/confirmation',    to: 'api/v1/auth/confirmations#show',               as: 'auth_confirmation'
-        post 'auth/sign_in',         to: 'api/v1/auth/sessions#create',                  as: 'create_auth_session'
-        post 'auth/guest_sign_in',   to: 'api/v1/auth/sessions#create_guest',            as: 'create_guest_auth_session'
-        post 'auth/sign_out',        to: 'api/v1/auth/sessions#destroy',                 as: 'destroy_auth_session'
-        post 'auth/unlock',          to: 'api/v1/auth/unlocks#create',                   as: 'create_auth_unlock'
-        get  'auth/unlock',          to: 'api/v1/auth/unlocks#show',                     as: 'auth_unlock'
-        post 'auth/password',        to: 'api/v1/auth/passwords#create',                 as: 'create_auth_password'
-        get  'auth/password',        to: 'api/v1/auth/passwords#edit',                   as: 'edit_auth_password'
-        post 'auth/password/update', to: 'api/v1/auth/passwords#update',                 as: 'update_auth_password'
-        get  'auth/validate_token',  to: 'api/v1/auth/token_validations#validate_token', as: 'auth_validate_token'
-      end
-    end
   end
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
---
+++ back/config/routes/admin.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      # [管理者権限]
+      namespace :admin do
+        # ユーザー
+        get  'users',                      to: 'users#index',          as: 'users'
+        get  'users/search',               to: 'users#search',         as: 'search_users'
+        post 'users/:username/delete',     to: 'users#destroy',        as: 'destroy_users'
+
+        # ジャンル
+        post 'genres/create',       to: 'genres#create',       as: 'create_genre'
+        post 'genres/:id/update',   to: 'genres#update',       as: 'update_genre'
+        post 'genres/:id/delete',   to: 'genres#destroy',      as: 'destroy_genre'
+
+        # お知らせ
+        get  'infomations',            to: 'infomations#index',     as: 'infomations'
+        post 'infomations/create',     to: 'infomations#create',    as: 'create_infomation'
+        post 'infomations/:id/delete', to: 'infomations#destroy',   as: 'destroy_infomation'
+
+        # 防災タスク
+        post 'tasks/create',       to: 'tasks#create',       as: 'create_task'
+        post 'tasks/:id/update',   to: 'tasks#update',       as: 'update_task'
+        post 'tasks/:id/delete',   to: 'tasks#destroy',      as: 'destroy_task'
+      end
+    end
+  end
+end
