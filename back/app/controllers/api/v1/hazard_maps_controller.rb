737bfef4a93c163e7d2702342d1256319e23f618
kazuki-ayimon <nomikazyu@gmail.com>
2022-11-23 16:06:46 +0900

    refactor: hazard_mapのapiをリファクタリング

--- back/app/controllers/api/v1/hazard_maps_controller.rb
+++ back/app/controllers/api/v1/hazard_maps_controller.rb
@@ -2,15 +2,8 @@ class Api::V1::HazardMapsController < Api::V1::ApplicationController
 
   # GET /api/v1/hazard_map(.json) ハザードマップ詳細API
   def show
-    if params[:prefecture_id].present?
-      @prefecture = Prefecture.find(params[:prefecture_id]).name
-      @city = City.find(params[:id]).name
-    else
-      @prefecture = Prefecture.find(current_user.prefecture_id).name
-      @city = City.find(current_user.city_id).name
-    end
-
-    @city_name = @prefecture + @city
-    @hazard_map = City.find(params[:id]).hazard_map
+    @prefecture = Prefecture.find(params[:prefecture_id])
+    @city = City.find(params[:city_id])
+    @hazard_map = @city.hazard_map
   end
 end
